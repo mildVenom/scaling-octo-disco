2cb20e1b37ed2d7303b2cfb6c3cc098159ed3511
Antony Gakuru <muriithigakuru@gmail.com>
2024-01-22 03:43:59 +0300

    remix week cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Chat from './pages/Chat';
 import Registration from './pages/Registration';
 import Schedule from './pages/Schedule';
+import WeekView from './pages/WeekView';
 function App() {
   return (
     <div className="app">
       {/* <Chat /> */}
       {/* <Registration /> */}
-      <Schedule />
+      {/* <Schedule /> */}
+      <WeekView />
     </div>
   );
 }
--- src/pages/WeekView.jsx
+++ src/pages/WeekView.jsx
@@ -2,7 +2,143 @@ import React from 'react'
 
 function WeekView() {
   return (
-    <section className='week-view container-flud'>WeekView</section>
+    <section className='week-view container p-0'>
+      {/* House of horrors */}
+      <div className="week-top top-day d-flex m-0 p-0">
+        <div className="week-top-cards week-card d-flex align-items-center justify-content-center text-light">Week 4</div>
+        <div className="week-top-cards d-flex align-items-center justify-content-center text-light">Sun</div>
+        <div className="week-top-cards d-flex align-items-center justify-content-center text-light">Mon</div>
+        <div className="week-top-cards d-flex align-items-center justify-content-center text-light">Tue</div>
+        <div className="week-top-cards d-flex align-items-center justify-content-center text-light">Wed</div>
+        <div className="week-top-cards d-flex align-items-center justify-content-center text-light">Thur</div>
+        <div className="week-top-cards d-flex align-items-center justify-content-center text-light">Fri</div>
+        <div className="week-top-cards d-flex align-items-center justify-content-center text-light">Sat</div>
+      </div>
+
+      <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">1</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>
+      <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">2</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>
+      <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">3</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>
+      <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">4</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>        <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">5</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>        <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">6</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>        <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">7</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>
+      <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">8</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>        <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">9</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>        <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">10</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>        <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">11</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light">cool</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>        <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">12</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>        <div className="day-top d-flex m-0 p-0">
+        <div className="day-cards day-card d-flex align-items-center justify-content-center text-light">13</div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+        <div className="day-cards d-flex align-items-center justify-content-center text-light"></div>
+      </div>
+
+    </section>
   )
 }
 
